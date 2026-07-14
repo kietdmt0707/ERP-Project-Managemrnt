dd7f79c73f6dda37b236f274d16eb2c823e3e4d9
kietdmt <kietdmt07@gmail.com>
2026-07-14 18:22:01 +0700

    Create SettingModels.cs

---
+++ backend/AronErpPm.Api/Models/SettingModels.cs
@@ -0,0 +1,38 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AronErpPm.Api.Models
+{
+    [Table("system_settings")]
+    public class SystemSetting
+    {
+        [Key]
+        public int SettingId { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string AppName { get; set; } = "ARON Project Management";
+
+        [MaxLength(500)]
+        public string? LogoUrl { get; set; }
+
+        [MaxLength(500)]
+        public string? BannerUrl { get; set; }
+
+        [MaxLength(250)]
+        public string? SmtpHost { get; set; }
+
+        public int SmtpPort { get; set; } = 587;
+
+        [MaxLength(250)]
+        public string? SmtpUsername { get; set; }
+
+        [MaxLength(250)]
+        public string? SmtpPassword { get; set; }
+
+        public bool SmtpEnableSsl { get; set; } = true;
+
+        public DateTime UpdatedDate { get; set; } = DateTime.UtcNow;
+    }
+}
