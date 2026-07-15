3ad6c12d7d5955519fa07a7f7e900033b47a7502
kietdmt <kietdmt07@gmail.com>
2026-07-15 13:27:02 +0700

    all
    Signed-off-by: kietdmt <kietdmt07@gmail.com>

--- backend/AronErpPm.Api/Models/OracleErpModels.cs
+++ backend/AronErpPm.Api/Models/OracleErpModels.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace AronErpPm.Api.Models
 {
     // 1. RICEFW Registry & Progress Tracking
+    [Table("ricefwregistry")]
     public class RicefwRegistry
     {
         [Key]
